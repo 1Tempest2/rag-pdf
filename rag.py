da622de5fd2c46091284e8d28157164151c8472f
Bábinszki Botond <137717060+1Tempest2@users.noreply.github.com>
2025-04-25 20:41:39 +0200

    upload/load file

--- rag.py
+++ rag.py
@@ -13,7 +13,6 @@ load_dotenv()
 
 pdf_directory = "./pdfs"
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-vector_db = FAISS()
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 pipeline = transformers.pipeline(
     "text-generation",
@@ -21,6 +20,24 @@ pipeline = transformers.pipeline(
     model_kwargs={"torch_dtype": torch.bfloat16, "load_in_4bit":True},
     device_map="auto",
 )
+
+def upload_pdf(new_pdf):
+    with open(pdf_directory + new_pdf.name, "wb") as f:
+        f.write(new_pdf.getbuffer())
+
+
+def load_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+    return documents
+
+
+def text_splitter(documents):
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1024,
+        chunk_overlap=256,
+        add_start_index=True
+    )
 
 
 
